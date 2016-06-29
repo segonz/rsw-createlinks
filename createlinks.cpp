c08f456cbf03687b02409a6200ca82fbf8b670fc
segonz <segonz@users.noreply.github.com>
2016-06-29 08:22:38 -0700

    Update createlinks.cpp

--- createlinks.cpp
+++ createlinks.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 
 int main(){
-	//pull list of item numbers into vector of strings
+
 	const string LINK = "http://shopalphacomm.com/";
 	vector<string> items;
 	string input_item;
@@ -20,32 +20,41 @@ int main(){
 	ifstream fin;
 	ofstream fout;
 
+	//open a list of vendor skus
 	fin.open("items.txt");
 	while (fin >> input_item){
+		//add base url to vedor sku
 		string appendlink = LINK + input_item;
 		items.push_back(appendlink);
 	}
 	fin.close();
 
+	//create a backup list
 	fout.open("linklist.csv");
 	for (int i = 0; i < items.size(); i++){
 		fout << LINK << items[i] << "\n";
 	}
 	fout.close();
-
+	
+	//declare where to start on master list - For beginning, enter 0
 	cout << "Start From? ";
 	cin >> count;
 	while (count > items.size() - 1){
 		cout << "Invalid. Enter Number less than " << items.size() << ": ";
 		cin >> count;
 	}
-
+	
+	
+	//opens up browswer window for each link, using default browser
+	//if default browser is open, will open new tabs
+	//otherwise, will open new windows
 	while ((continue_open) && (count < items.size())){
 		string open_file = string("start ").append(items[count]);
 		string cont_yn;
 		system(open_file.c_str());
 		cout << count + 1 << ". " << items[count] << "\n";
 		count++;
+		//open 10 at a time
 		if (count % 10 == 0){
 			cout << "\nContinue? ";
 			cin >> cont_yn;
